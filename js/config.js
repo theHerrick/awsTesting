dce2e2be63464835955467f4c98f55605ba99c13
Simon Herrick <simon@herrick.io>
2023-06-14 20:09:59 +0100

    formatting error

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
         userPoolId: 'eu-west-2_kP4yzaLzd', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '	28lapbscuc9dfhepml7kvt03rc', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientId: '28lapbscuc9dfhepml7kvt03rc', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'eu-west-2' // e.g. us-east-2
     },
     api: {
